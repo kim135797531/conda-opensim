0da508a7c7c5c9cdc1a22546bb0c120857173787
Dongmin Kim <d-kim@isi.imi.i.u-tokyo.ac.jp>
2019-12-10 20:02:46 +0900

    Use a latest OpenSim and Build as RelWithDebInfo

--- opensim/build.sh
+++ opensim/build.sh
@@ -7,8 +7,8 @@ mkdir opensim_dependencies_build
 cd opensim_dependencies_build
 cmake ../dependencies/ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_BUILD_TYPE=Release
-make -j8
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo
+make -j24
 cd ..
 
 cp -r $PREFIX/BTK/lib/btk-0.4dev/* $PREFIX/lib/
@@ -19,12 +19,12 @@ mkdir opensim_build
 cd opensim_build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DOPENSIM_DEPENDENCIES_DIR="$PREFIX" \
       -DBUILD_PYTHON_WRAPPING=ON \
       -DBUILD_JAVA_WRAPPING=OFF \
       -DPYTHON_VERSION_MAJOR=3 \
       -DPYTHON_VERSION_MINOR=6 \
       -DWITH_BTK=ON
-make -j8
+make -j24
 make install
