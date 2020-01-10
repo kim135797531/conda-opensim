40566ff0715ab0cc0047468e932212ddccb2b847
Dongmin Kim <d-kim@isi.imi.i.u-tokyo.ac.jp>
2020-01-10 16:41:51 +0900

    Support for using build cache

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # conda-opensim
-Unofficial conda recipe for OpenSim 4.0
+Unofficial conda recipe for OpenSim 4.1
+https://anaconda.org/kim135797531/opensim-nightly
 
-Usage:
+(For End-User)
+Install:
+
+    conda install -c kim135797531 opensim-nightly
+
+(For Developer)
+Prepare:
+
+    conda env with python3.6
+
+Build:
 
     cd conda-opensim
-    conda build opensim
-    
-For python 3, use the python3 branch and 
-
     conda build opensim --python 3.6
+
+(For debugging)
+
+    # 그냥 source 하면 빌드 폴더를 새로 만든다. (재활용 불가)
+    # 그냥 dirty 하면 source는 안 가져 온다. (원본 코드 수정 반영 불가)
+    conda build opensim --python=3.6 --source --dirty
+
+    # source 업뎃 후에 빌드를 해야 한다.
+    conda build opensim --python=3.6 --no-test --dirty
--- opensim/build.sh
+++ opensim/build.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # TODO: Dependencies should be separate packages...
-mkdir opensim_dependencies_build
+mkdir -p opensim_dependencies_build
 cd opensim_dependencies_build
 cmake ../dependencies/ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
@@ -15,7 +15,7 @@ cp -r $PREFIX/BTK/lib/btk-0.4dev/* $PREFIX/lib/
 cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
 
 # TODO: Tests are missing!
-mkdir opensim_build
+mkdir -p opensim_build
 cd opensim_build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
@@ -23,6 +23,7 @@ cmake ../ \
       -DOPENSIM_DEPENDENCIES_DIR="$PREFIX" \
       -DBUILD_PYTHON_WRAPPING=ON \
       -DBUILD_JAVA_WRAPPING=OFF \
+      -DOPENSIM_PYTHON_VERSION=3 \
       -DPYTHON_VERSION_MAJOR=3 \
       -DPYTHON_VERSION_MINOR=6 \
       -DWITH_BTK=ON
